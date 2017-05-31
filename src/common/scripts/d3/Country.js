793b9500bebf9c2196e0fb3eaa575c5a878bcd1b
Omar Torres <otorres@Omars-MacBook-Pro.local>
2017-05-31 17:33:06 -0500

    Avoid double rednering from paths
    Paths not updating but restore zoom fails

--- src/common/scripts/d3/Country.js
+++ src/common/scripts/d3/Country.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { geoMercator, geoAlbersUsa, geoPath } from 'd3-geo';
-import { mesh, merge, feature } from 'topojson';
 
-const Country = ({ className, d, onClick, selected }) => {
-  const selectedClass = selected ? 'country--selected' : '';
-  const classes = [className, selectedClass].join(' ');
-  return (
-    <path 
-      className={classes}
-      d={d}
-      onClick={onClick}
-    />
-  );
+class Country extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.POP_CNTRY !== this.props.POP_CNTRY;
+  }
+
+  render() {
+    console.log('rendering');
+    const { className, d, onClick, selected } = this.props;
+    const selectedClass = selected ? 'country--selected' : '';
+    const classes = [className, selectedClass].join(' ');
+    return (
+      <path 
+        className={classes}
+        d={d}
+        onClick={onClick}
+      />
+    );
+  }
 };
+// const Country = ({ className, d, onClick, selected }) => {
+//   const selectedClass = selected ? 'country--selected' : '';
+//   const classes = [className, selectedClass].join(' ');
+//   return (
+//     <path 
+//       className={classes}
+//       d={d}
+//       onClick={onClick}
+//     />
+//   );
+// };
 
 export default Country;
 
